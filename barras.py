a0c1d03c63178ec01a5abfd1403be2b0723f25ef
Otavio Zucheratto <ommz21@ufmg.br>
2020-04-17 15:20:03 -0300

    Primeiras aulas do curso.

---
+++ barras.py
@@ -0,0 +1,17 @@
+# Visualização de dados em Python
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 7, 1, 0]
+
+titulo = "Gráfico de barras"
+eixox = "Eixo X"
+eixoy = "Eixo Y"
+
+# Legendas
+plt.title(titulo)
+plt.xlabel(eixox)
+plt.ylabel(eixoy)
+
+plt.bar(x, y)
+plt.show()
